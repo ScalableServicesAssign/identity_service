78952b5477cb2e0354c01fa36893f3aef2962354
Sharanya <learningautomation27@gmail.com>
2024-11-20 21:49:53 +0530

    Added some code as part of security

--- identity-service/src/main/java/com/bitswilp/langlearn/identityService/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/bitswilp/langlearn/identityService/IdentityServiceApplication.java
@@ -4,8 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "Security Microservice Swagger",
+	        version = "v1",
+	        description = "API documentation for My Microservice"
+	    )
+	)
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
--- identity-service/src/main/java/com/bitswilp/langlearn/identityService/config/AuthConfig.java
+++ identity-service/src/main/java/com/bitswilp/langlearn/identityService/config/AuthConfig.java
@@ -1,5 +1,7 @@
 package com.bitswilp.langlearn.identityService.config;
 
+import java.beans.Customizer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,19 +19,32 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class AuthConfig {
+	public static final String[] ENDPOINTS_WHITELIST = {
+			"/auth/**",
+			"/languagelearning",
+            "/api/user/signup",
+            "/v3/api-docs.yaml",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui/index.html",
+            "/api/v1/**"
+    };
 
     @Bean
     public UserDetailsService userDetailsService(){
         return new CustomUserDetailsService();
     }
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/auth/register", "/auth/token", "/auth/validate").permitAll()
-                .and()
+    	return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        //.requestMatchers("/api/auth/**").permitAll() //Can check for Roles here
+                        .requestMatchers(ENDPOINTS_WHITELIST).permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 
